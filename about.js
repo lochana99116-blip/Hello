7722866f5c1e6431f9afa80eccc517701fb2825d
lochana99116-blip <lochana99116@gmail.com>
2026-05-25 15:37:00 +0530

    Update about.js

--- about.js
+++ about.js
@@ -1,3 +1,18 @@
+// SIDE BAR
+document.addEventListener("DOMContentLoaded", function () {
+
+  const toggle = document.getElementById("menu-toggle");
+  const nav = document.getElementById("nav-links");
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", function () {
+      nav.classList.toggle("show");
+    });
+  }
+
+});
+
+
 // NAVBAR SCROLL EFFECT
 
 window.addEventListener("scroll", () => {
